c52235b81b9db41525bf1fcf8bdba59a5dfd4603
55797559 <55797559+jackgroom@users.noreply.github.com>
2021-03-12 21:34:32 +0000

    setting up the main game loop

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from player import Player
 
 
 def main():
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        clock.tick(60)
+        redraw_gameWindow()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+
+def redraw_gameWindow():
     pass
 
 
